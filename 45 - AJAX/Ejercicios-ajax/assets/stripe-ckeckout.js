0eac599439eaefda301a5beaebc0e6e989131a51
ZakyUp <andresjr195@hotmail.com>
2022-04-04 21:23:52 -0500

    fetch-anidado

--- 45 - AJAX/Ejercicios-ajax/assets/stripe-ckeckout.js
+++ 45 - AJAX/Ejercicios-ajax/assets/stripe-ckeckout.js
@@ -1,3 +1,4 @@
+import stripeKeys from "./stripe-keys.js";
 import STRIPE_KEYS from "./stripe-keys.js";
 
 const d = document,
@@ -12,6 +13,7 @@ fetchOptions = {
 };
 
 let products, prices;
+const moneyFormat = (num) => `$${num.slice(0, -2)}.${num.slice(-2)}`;
 
 Promise.all([
     fetch("https://api.stripe.com/v1/products",fetchOptions),
@@ -22,18 +24,18 @@ Promise.all([
     //console.log(json);
     products = json[0].data;
     prices = json[1].data;
-    console.log(products, prices);
+    //console.log(products, prices);
 
     prices.forEach((el) => {
         let productData = products.filter((product) => product.id === el.product);
-        console.log(productData);
+        //console.log(productData);
 
         $template.querySelector(".plato").setAttribute("data-price", el.id);
         $template.querySelector("img").src = productData[0].images[0];
         $template.querySelector("img").alt = productData[0].name;
         $template.querySelector("figcaption").innerHTML = `${productData[0].name}
         <br>
-        ${el.unit_amount_decimal} ${el.currency}`;
+        ${moneyFormat(el.unit_amount_decimal)} ${el.currency}`;
         
 
         let $clone = d.importNode($template, true);
@@ -44,7 +46,27 @@ Promise.all([
 })
 .catch((err) => {
     console.log(err);
-    let message = err.statusText || "Ocurrir un error al conectarse con la API de Stripe"
+    let message = err.statusText || "Ocurrio un error al conectarse con la API de Stripe"
 
     $platos.innerHTML = `<p>Error: ${err.status}: ${message}</p>`
-})
+});
+
+d.addEventListener("click", (e) => {
+    //console.log(e.target);
+    if(e.target.matches(".plato *")){
+        let price = e.target.parentElement.getAttribute("data-price");
+        //console.log(price);
+        Stripe(STRIPE_KEYS.public)
+        .redirectToCheckout({
+            lineItems:[{price, quantity: 1}],
+            mode: "subscription",
+            successUrl: "http://127.0.0.1:5500/45%20-%20AJAX/Ejercicios-ajax/assets/stripe-success.html",
+            cancelUrl: "http://127.0.0.1:5500/45%20-%20AJAX/Ejercicios-ajax/assets/stripe-cancel.html",
+        })
+        .then((res) => {
+            if(res.error){
+                $platos.insertAdjacentHTML("afterend", res.error.message);
+            }
+        });
+    }
+});
